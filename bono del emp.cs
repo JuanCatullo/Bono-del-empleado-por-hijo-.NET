f9f379740142dbeedf7a79c611b00f2b4af64896
Juan <101681675+JuanCatullo@users.noreply.github.com>
2022-07-24 14:51:21 -0300

    Add files via upload

---
+++ bono del emp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bono_del_empleado_por_hijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SUELDO, ARECIBIR;
+            int NHIJO, BONO;
+            string NOM, linea;
+            Console.WriteLine("Nombre Empleado :");
+            NOM = Console.ReadLine();
+            Console.WriteLine("Sueldo Empleado :");
+            linea = Console.ReadLine();
+            SUELDO = Double.Parse(linea);
+            Console.WriteLine("Numero de Hijos :");
+            linea = Console.ReadLine();
+            NHIJO = int.Parse(linea);
+            if (NHIJO >= 3) 
+            { BONO = NHIJO * 10;  
+            
+            } 
+            else
+            {
+                BONO = NHIJO * 20;
+            }
+            
+            ARECIBIR = SUELDO + BONO;
+
+            Console.WriteLine ("Recibe :" + string.Format ("{0:c}", ARECIBIR));
+            Console.WriteLine("Pulse una tecla");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
